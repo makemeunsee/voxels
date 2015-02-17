4c6155afb83128cffc4dd1084f3e7d532c7a1ab3
makemeunsee <makemeunsee@gmail.com>
2015-02-17 01:01:55 +0100

    sphere maze, scala js port: repo init

---
+++ src/main/scala/demo/webapp/TutoMain.scala
@@ -0,0 +1,12 @@
+package demo.webapp
+
+/**
+ * Created by markus on 16/02/2015.
+ */
+import scala.scalajs.js.JSApp
+
+object TutoMain extends JSApp {
+  def main(): Unit = {
+    println("Hello world!")
+  }
+}
