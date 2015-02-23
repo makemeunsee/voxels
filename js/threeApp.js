d7a1fc7a7cc05fa5e8019a2d4ae5d33297fe8b05
makemeunsee <makemeunsee@gmail.com>
2015-02-24 00:48:06 +0100

    model view projection matrices support + dummy cube model

---
+++ js/threeApp.js
@@ -0,0 +1,472 @@
+function getURLParameter(sParam) {
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
+        }
+    }
+}
+
+function arrToMat( arr ) {
+    var mat = new THREE.Matrix4();
+    mat.set( arr[0][0], arr[0][1], arr[0][2], arr[0][3],
+             arr[1][0], arr[1][1], arr[1][2], arr[1][3],
+             arr[2][0], arr[2][1], arr[2][2], arr[2][3],
+             arr[3][0], arr[3][1], arr[3][2], arr[3][3] );
+    return mat;
+}
+
+function makeMesh( model ) {
+    var customUniforms = {
+        u_time: { type: "1f", value: 0 },
+        u_depthMode: { type: "1f", value: 0 },
+        u_depthScale: { type: "1f", value: 0 },
+        u_explodedFactor: { type: "1f", value: 0 },
+        u_borderWidth: { type: "1f", value: 0 },
+        u_mvpMat: { type: "m4", value: new THREE.Matrix4() },
+        u_color: { type: "v4", value: new THREE.Vector4( 1, 1, 1, 1 ) },
+        u_borderColor: { type: "v4", value: new THREE.Vector4( 0, 0, 0, 1 ) }
+    };
+
+    var geometry = new THREE.BufferGeometry();
+
+    // create attributes for each vertex
+    // currently 2 colors are given as vertex attributes
+    var attributes = {
+        a_normal: {
+             type: 'v3',
+             value: []
+        },
+        a_centerFlag: {
+            type: 'f',
+            value: []
+        },
+        a_mazeDepth: {
+            type: 'f',
+            value: []
+        }
+    };
+
+    var shaderMaterial = new THREE.ShaderMaterial({
+        attributes:     attributes,
+        uniforms:       customUniforms,
+        vertexShader:   document.getElementById('shader-vs').innerHTML,
+        fragmentShader: document.getElementById('shader-fs').innerHTML,
+        side: THREE.DoubleSide
+    });
+
+    var positions = new Float32Array( model.vertice.length );
+    var normals = new Float32Array( model.vertice.length );
+    var centerFlag = new Float32Array( model.vertice.length / 3 );
+    var mazeDepth = new Float32Array( model.vertice.length / 3 );
+    for (var i = 0; i < model.vertice.length / 3; i++) {
+        positions [ 3*i ] = model.vertice[ 3*i ];
+        normals [ 3*i ] = model.normals[ 3*i ];
+        positions [ 3*i+1 ] = model.vertice[ 3*i+1 ];
+        normals [ 3*i+1 ] = model.normals[ 3*i+1 ];
+        positions [ 3*i+2 ] = model.vertice[ 3*i+2 ];
+        normals [ 3*i+2 ] = model.normals[ 3*i+2 ];
+        centerFlag[ i ] = model.centers[ i ];
+        mazeDepth[ i ] = model.mazeData[ i ];
+    }
+    var indices = new Uint16Array( model.indice.length );
+    for (var i = 0; i < model.indice.length ; i++) {
+        indices[ i ] = model.indice[ i ];
+    }
+
+    geometry.addAttribute( 'index', new THREE.BufferAttribute( indices, 1 ) );
+    geometry.addAttribute( 'a_mazeDepth', new THREE.BufferAttribute( mazeDepth, 1 ) );
+    geometry.addAttribute( 'a_centerFlag', new THREE.BufferAttribute( centerFlag, 1 ) );
+    geometry.addAttribute( 'a_normal', new THREE.BufferAttribute( normals, 3 ) );
+    geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+
+    return new THREE.Mesh( geometry, shaderMaterial );
+}
+
+function modelFromRaw( model ) {
+    return {
+        "vertice": model[0],
+        "normals": model[1],
+        "centers": model[2],
+        "mazeData": model[3],
+        "indice": model[4],
+    };
+}
+
+
+DummyCamera = function () {
+    THREE.Camera.call( this );
+    this.type = 'DummyCamera';
+};
+
+DummyCamera.prototype = Object.create( THREE.Camera.prototype );
+
+function appMain() {
+
+    var seed = getURLParameter("seed") || Math.random().toString().slice(2);
+    console.log("seed:\t", seed);
+
+    function rndSpherePosition() {
+        var u = Math.random();
+        var v = Math.random();
+        return [ 2*Math.PI*u, Math.acos( 2*v - 1 ) ];
+    }
+
+    // help dialog
+    $(function() {
+        $( "#dialog" ).dialog({
+            width: 600,
+            height: 180
+        });
+    });
+    $( "#dialog" ).dialog( "close" );
+
+    function toggleUI() {
+        console.log("toggling ui");
+        if ( $( "#gui" ).css( "display" ) === "none" )
+            $( "#gui" ).show();
+        else
+            $( "#gui" ).hide();
+    }
+
+    // jqueryui widgets
+    $(function() {
+      $( "button" ).button();
+    });
+
+    var depthMaze = true;
+    function flatFct() {
+        depthMaze = !depthMaze;
+    }
+    $("#flat").unbind("click");
+    $("#flat").click(flatFct);
+    $("#flat").attr("checked", false);
+
+    var reverseDepth = false;
+    function reverseDepthFct() {
+        reverseDepth = !reverseDepth;
+    }
+    $("#reverseDepth").unbind("click");
+    $("#reverseDepth").click(reverseDepthFct);
+    $("#reverseDepth").attr("checked", false);
+
+    function showHelp() {
+        if ( $( "#dialog" ).dialog( "isOpen" ) )
+            $( "#dialog" ).dialog( "close" );
+        else
+            $( "#dialog" ).dialog( "open" );
+    }
+    $("#showHelp").unbind("click");
+    $("#showHelp").click(showHelp);
+
+    var depthExagg = 0.5;
+    var explosion = 0.0;
+
+    function screenshot() {
+        var w = window.open('', '');
+        w.document.title = "Screenshot";
+        w.document.body.style.backgroundColor = "black";
+        var img = new Image();
+        img.src = canvas.toDataURL("image/png");
+        w.document.body.appendChild(img);
+    }
+    $("#screenshot").unbind("click");
+    $("#screenshot").click(screenshot);
+
+    var toggleFullscreen = THREEx.FullScreen.toggleFct();
+    $("#fullscreen").unbind("click");
+    $("#fullscreen").click(toggleFullscreen);
+
+    var cuts = getURLParameter("cells") || 0;
+    console.log( "cells:\t", cuts );
+    var cutPositions = new Array(2*cuts);
+    for ( var i = 0; i < cuts; i++ ) {
+        var cut = rndSpherePosition();
+        cutPositions[2*i] = cut[0];
+        cutPositions[2*i+1] = cut[1];
+    }
+
+    var cubePlanet = [
+       [ 1,1,1
+       , -1,1,1
+       , -1,1,-1
+       , 1,1,-1
+       , -1,-1,-1
+       , 1,-1,-1
+       , 1,-1,1
+       , -1,-1,1
+       , 0,1,0
+       , 0,-1,0
+       , 1,0,0
+       , -1,0,0
+       , 0,0,1
+       , 0,0,-1
+       ],
+       [ Math.sqrt(3)/3,Math.sqrt(3)/3,Math.sqrt(3)/3
+       , -Math.sqrt(3)/3,Math.sqrt(3)/3,Math.sqrt(3)/3
+       , -Math.sqrt(3)/3,Math.sqrt(3)/3,-Math.sqrt(3)/3
+       , Math.sqrt(3)/3,Math.sqrt(3)/3,-Math.sqrt(3)/3
+       , -Math.sqrt(3)/3,-Math.sqrt(3)/3,-Math.sqrt(3)/3
+       , Math.sqrt(3)/3,-Math.sqrt(3)/3,-Math.sqrt(3)/3
+       , Math.sqrt(3)/3,-Math.sqrt(3)/3,Math.sqrt(3)/3
+       , -Math.sqrt(3)/3,-Math.sqrt(3)/3,Math.sqrt(3)/3
+       , 0,1,0
+       , 0,-1,0
+       , 1,0,0
+       , -1,0,0
+       , 0,0,1
+       , 0,0,-1
+       ],
+       [0,0,0,0,0,0,0,0,1,1,1,1,1,1],
+       [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+       [8,0,3,8,3,2,8,2,1,8,1,0
+       ,9,4,5,9,5,6,9,6,7,9,7,4
+       ,10,0,6,10,6,5,10,5,3,10,3,0
+       ,11,1,2,11,2,4,11,4,7,11,7,1
+       ,12,0,1,12,1,7,12,7,6,12,6,0
+       ,13,2,3,13,3,5,13,5,4,13,4,2]
+    ];
+
+    var id = getURLParameter( "polyId" ) || 3;
+    console.log( "polyId:\t", id );
+    var overlapThreshold = getURLParameter( "overlapThreshold" ) || 100;
+    console.log( "overlapThreshold:\t", overlapThreshold );
+    var model = modelFromRaw ( cubePlanet );
+
+    var zoomMax = 16;
+    var zoomMin = 0.0625;
+    var zoomSpeed = 1.01;
+    var zoom = 1;
+
+    var dummyCam = new DummyCamera();
+    var camTheta = Math.PI / 2;
+    var camPhi = Math.PI / 2;
+    var camDist = 1;
+
+    var projMat = new THREE.Matrix4();
+    var viewMat = arrToMat( demo.webapp.TutoMain().viewMatOf( camTheta, camPhi, camDist ) );
+    var modelMat = new THREE.Matrix4();
+    modelMat.elements[0] = Math.sqrt(2) / 2;
+    modelMat.elements[2] = Math.sqrt(2) / 2;
+    modelMat.elements[5] = 1;
+    modelMat.elements[8] = -Math.sqrt(2) / 2;
+    modelMat.elements[10] = Math.sqrt(2) / 2;
+    var scaledModelMat = modelMat.clone();
+
+    var mvp = new THREE.Matrix4();
+    function updateMVPs() {
+        var p = projMat.clone();
+        var vp = p.multiply( viewMat );
+        scaledModelMat = new THREE.Matrix4();
+        scaledModelMat.multiplyScalar( zoom ).multiply( modelMat );
+        scaledModelMat.elements[15] = 1;
+        mvp = vp.multiply( scaledModelMat );
+    }
+
+    var updateProjection = function(screenWidth, screenHeight) {
+        projMat = arrToMat( demo.webapp.TutoMain().orthoMatrixFromScreen( screenWidth, screenHeight, 1.75 ) );
+        updateMVPs();
+    };
+
+    updateProjection(window.innerWidth, window.innerHeight);
+
+    var currentMesh =  makeMesh( model );
+
+    var mainContainer = document.getElementById( 'main' );
+
+    // pinch detection (and more)
+    var mc = new Hammer(mainContainer);
+    mc.get("pinch").set({ enable: true });
+
+    mc.on("pinch", function(ev) {
+        zoomFct(ev.scale < 1 ? -1 : 1, 1.04);
+    });
+
+    var scene = new THREE.Scene();
+    var renderer = new THREE.WebGLRenderer( { preserveDrawingBuffer: true } );
+    var canvas = renderer.domElement;
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    mainContainer.appendChild( canvas );
+
+    scene.add( currentMesh );
+
+    function leftButton(evt) {
+        var button = evt.which || evt.button;
+        return button == 1;
+    }
+
+    var mx, my = 0;
+
+    var tapped = false; // double tap detection
+
+    function onMouseDown(event) {
+        if (leftButton(event)) {
+            event.touches = [{clientX: event.clientX, clientY: event.clientY}];
+            onTouchStart(event);
+            clearTimeout(tapped);
+            tapped = null; // double click is handled natively
+        }
+    }
+
+    // only react to left clicks
+    function onTouchStart(event) {
+        // dont handle multi touch
+        if (event.touches.length === 1) {
+            mx = event.touches[0].clientX;
+            my = event.touches[0].clientY;
+            canvas.addEventListener( "mouseup", onMouseUp, false );
+            canvas.addEventListener( "touchend", onTouchEnd, false );
+            canvas.addEventListener( "mousemove", onMouseMove, false );
+            canvas.addEventListener( "touchmove", onTouchMove, false );
+            if(!tapped){ //if tap is not set, set up single tap
+                tapped = setTimeout(function() {
+                    tapped = null
+                }, 300);   //wait 300ms then run single click code
+            } else {    //tapped within 300ms of last tap. double tap
+              clearTimeout(tapped); //stop single tap callback
+              tapped = null;
+              toggleUI()
+            }
+        }
+    }
+
+    function onMouseUp(event) {
+        if (leftButton(event)) {
+            onTouchEnd(event);
+        }
+    }
+
+    function onTouchEnd(event) {
+        canvas.removeEventListener( "mouseup", onMouseUp, false );
+        canvas.removeEventListener( "touchend", onTouchEnd, false );
+        canvas.removeEventListener( "mousemove", onMouseMove, false );
+        canvas.removeEventListener( "touchmove", onTouchMove, false );
+    }
+
+    function onMouseMove(event) {
+        event.touches = [{clientX: event.clientX, clientY: event.clientY}];
+        onTouchMove(event);
+    }
+
+    // mouse drag -> move camera (adjusted to zoom)
+    function onTouchMove(event) {
+        // dont handle multi touch
+        if (event.touches.length === 1) {
+            event.preventDefault();
+            var deltaX = event.touches[0].clientX - mx;
+            var deltaY = event.touches[0].clientY - my;
+
+            modelMat = arrToMat( demo.webapp.TutoMain().naiveRotMat( deltaX * 0.005, deltaY * 0.005 ) ).multiply( modelMat );
+            updateMVPs();
+
+            mx = event.touches[0].clientX;
+            my = event.touches[0].clientY;
+            // no need to update cam, projection matrix is not changed by translation
+        }
+    }
+
+    function zoomFct(delta, alpha) {
+        zoom = Math.min( Math.max( zoom * ( Math.pow( alpha, delta ) ), zoomMin ), zoomMax );
+        updateMVPs();
+    }
+
+    // mouse wheel -> zoom in / out
+    function onMouseWheel(event) {
+        zoomFct( Math.max( -1, Math.min( 1, ( event.wheelDelta || -event.detail ) ) ), 1.05 );
+    }
+
+    canvas.addEventListener( "mousedown", onMouseDown, false );
+    canvas.addEventListener( "touchstart", onTouchStart, false );
+
+    canvas.addEventListener( "mousewheel", onMouseWheel, false );
+      // Firefox
+    canvas.addEventListener( "DOMMouseScroll", onMouseWheel, false );
+
+    canvas.addEventListener( "dblclick", toggleUI, false );
+
+    THREEx.WindowResize(renderer, updateProjection);
+    THREEx.FullScreen.bindKey({ charCode : 'f'.charCodeAt(0) });
+
+    // A cross-browser requestAnimationFrame
+    // See https://hacks.mozilla.org/2011/08/animating-with-javascript-from-setinterval-to-requestanimationframe/
+    var requestAnimFrame = (function() {
+        return window.requestAnimationFrame    ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            window.oRequestAnimationFrame      ||
+            window.msRequestAnimationFrame     ||
+            function(callback){
+                window.setTimeout(callback, 1000 / 60);
+            };
+    })();
+
+    // Don't run the game when the tab isn't visible
+    window.addEventListener('focus', function() {
+        unpause();
+    });
+
+    window.addEventListener('blur', function() {
+        pause();
+    });
+
+    reset();
+//    var then = Date.now();
+    var running = true;
+    main();
+
+
+    // Functions ---
+
+    // Reset game to original state
+    function reset() {
+    }
+
+    // Pause and unpause
+    function pause() {
+        running = false;
+    }
+
+    function unpause() {
+        running = true;
+//        then = Date.now();
+        main();
+    }
+
+    var simTime = 0.0;
+
+    // The main game loop
+    function main() {
+        if(!running) {
+            return;
+        }
+
+//        var now = Date.now();
+//        var dt = now - then;
+
+        if ( depthMaze && reverseDepth ) {
+            currentMesh.material.uniforms.u_depthMode.value = -1.0;
+        } else if ( depthMaze && !reverseDepth ) {
+            currentMesh.material.uniforms.u_depthMode.value = 1.0;
+        } else {
+            currentMesh.material.uniforms.u_depthMode.value = 0.0;
+        }
+        currentMesh.material.uniforms.u_explodedFactor.value = explosion;
+        currentMesh.material.uniforms.u_depthScale.value = depthExagg;
+        currentMesh.material.uniforms.u_time.value = simTime;
+        currentMesh.material.uniforms.u_borderWidth.value = 1.0;
+        currentMesh.material.uniforms.u_mvpMat.value = mvp;
+        currentMesh.material.uniforms.u_color.value = new THREE.Vector4(1,1,1,1);
+        currentMesh.material.uniforms.u_borderColor.value = new THREE.Vector4(0.05,0.05,0.05,1);
+
+        renderer.render(scene, dummyCam);
+
+//        then = now;
+        requestAnimFrame(main);
+    }
+
+
+};
+
+window.onload = appMain;
--- src/main/scala/demo/webapp/TutoMain.scala
+++ src/main/scala/demo/webapp/TutoMain.scala
@@ -4,9 +4,118 @@ package demo.webapp
  * Created by markus on 16/02/2015.
  */
 import scala.scalajs.js.JSApp
+import scala.scalajs.js.annotation.JSExport
+import scala.scalajs.js.Array
+
+case class Vec3( x: Double, y: Double, z: Double) {
+  def apply( i: Int ) = i match {
+    case 0 => x
+    case 1 => y
+    case 2 => z
+  }
+
+  def update( i: Int, v: Double ) = i match {
+    case 0 => copy( x = v )
+    case 1 => copy( y = v )
+    case 2 => copy( z = v )
+  }
+
+  def norm = math.sqrt( x*x + y*y + z*z )
+
+  def normalize = {
+    val n = norm
+    Vec3( x / n, y / n, z / n )
+  }
+
+  def minus( v: Vec3 ) = Vec3( x - v.x, y - v.y, z - v.z )
+  def plus( v: Vec3 ) = Vec3( x + v.x, y + v.y, z + v.z )
+  def dot( v: Vec3 ) = x*v.x + y*v.y + z*v.z
+  def cross( v: Vec3 ) =
+    Vec3( y*v.z - v.y*z,
+          z*v.x - v.z*x,
+          x*v.y - v.x*y )
+
+}
 
 object TutoMain extends JSApp {
   def main(): Unit = {
-    println("Hello world!")
+    println("start")
   }
+
+  def rotMat( angle: Double, vX: Double, vY: Double, vZ: Double ): Array[Array[Double]] = {
+    val n = math.sqrt( vX*vX + vY*vY + vZ*vZ )
+    val nX = vX / n
+    val nY = vY / n
+    val nZ = vZ / n
+    val cosA = math.cos( angle )
+    val sinA = math.sin( angle )
+    Array( Array( cosA + nX*nX*(1-cosA),    nX*nY*(1-cosA) - nZ*sinA, nX*nZ*(1-cosA) + nY*sinA, 0 )
+         , Array( nX*nY*(1-cosA) + nZ*sinA, cosA + nY*nY*(1-cosA),    nY*nZ*(1-cosA) - nX*sinA, 0 )
+         , Array( nX*nZ*(1-cosA) - nY*sinA, nY*nZ*(1-cosA) + nX*sinA, cosA + nZ*nZ*(1-cosA),    0 )
+         , Array( 0,                        0,                        0,                        1 ))
+  }
+
+  def multMat( m0: Array[Array[Double]], m1: Array[Array[Double]] ): Array[Array[Double]] = {
+    val r: Array[Array[Double]] = Array( Array(0,0,0,0), Array(0,0,0,0), Array(0,0,0,0), Array(0,0,0,0) )
+    for ( i <- 0 to 3; j <- 0 to 3 )
+      for ( k <- 0 to 3 )
+        r( i ).update( j, r ( i )( j ) + m0( i )( k ) * m1( k )( j ) )
+    r
+  }
+
+  @JSExport
+  def naiveRotMat(theta: Double, phi: Double): Array[Array[Double]] = {
+    multMat ( rotMat( theta, 0, 1, 0 ), rotMat( phi, 1, 0, 0 ) )
+  }
+
+  def latLongPosition(theta: Double, phi: Double, distance: Double): Vec3 = {
+    val cosP = math.cos( phi )
+    val sinP = math.sin( phi )
+    val cosT = math.cos( theta )
+    val sinT = math.sin( theta )
+    Vec3( distance * sinP * cosT, distance * cosP, distance * sinP * sinT )
+  }
+
+  def lookAtMatrix( eye: Vec3, target: Vec3, up: Vec3 ): Array[Array[Double]] = {
+    val forward = (target minus eye).normalize
+    val right = (forward cross up).normalize
+    val up0 =  right cross forward
+    Array( Array( right.x, right.y, right.z, -(right dot eye) ),
+           Array( up0.x, up0.y, up0.z, -(up0 dot eye) ),
+           Array( -forward.x, -forward.y, -forward.z, forward dot eye ),
+           Array( 0, 0, 0, 1 ) )
+  }
+
+  @JSExport
+  def viewMatOf( theta: Double, phi: Double, distance: Double): Array[Array[Double]] = {
+    val p = latLongPosition ( theta, math.max( 0.00001, math.min( math.Pi - 0.00001, phi ) ), distance )
+    lookAtMatrix( p, Vec3( 0, 0, 0 ), Vec3( 0, 1, 0 ) )
+  }
+
+  def orthoMatrix( left: Double, right: Double, bottom: Double, top: Double, near: Double, far: Double ): Array[Array[Double]] = {
+    val x_orth = 2 / (right - left)
+    val y_orth = 2 / (top - bottom)
+    val z_orth = -2 / (far - near)
+    val tx = -(right + left) / (right - left)
+    val ty = -(top + bottom) / (top - bottom)
+    val tz = -(far + near) / (far - near)
+    Array( Array( x_orth, 0, 0, tx ),
+           Array( 0, y_orth, 0, ty ),
+           Array( 0, 0, z_orth, tz ),
+           Array( 0, 0, 0, 1 ) )
+  }
+
+  @JSExport
+  def orthoMatrixFromScreen( w: Double, h: Double, k: Double ) = {
+    val hh = if ( h < 0 ) 1 else h
+    val aspect = w / hh
+    val far = 100
+    val near = -100
+    val right = k * aspect
+    val top = k
+    val left = -right
+    val bottom = -top
+    orthoMatrix( left, right, bottom, top, near, far )
+  }
+
 }
