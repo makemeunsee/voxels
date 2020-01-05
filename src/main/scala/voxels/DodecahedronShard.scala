68af56d12ab7a8425e96cf331de321493f4b65a3
makemeunsee <makemeunsee@gmail.com>
2020-01-05 23:11:55 +0100

    dodecahedron shard done

--- src/main/scala/voxels/DodecahedronShard.scala
+++ src/main/scala/voxels/DodecahedronShard.scala
@@ -9,22 +9,37 @@ import geometry.gold
  */
 object DodecahedronShard extends VoxelStandard {
 
-  val vertices: List[Vertex] =
-    Vec3( 1,0,-3/2-math.sqrt(5)/2 ) ::
-    Vec3( 0,0,-3/2-math.sqrt(5)/2 ) ::
-    Vec3( 3/4+math.sqrt(5)/4, 1/4 + math.sqrt(5)/4, -1-math.sqrt(5)/2 ) ::
-    Vec3( 3/4+math.sqrt(5)/4, -1/4 - math.sqrt(5)/4, -1-math.sqrt(5)/2 ) ::
-    Vec3( 0,1/2+3*math.sqrt(5)/10,-1-2*math.sqrt(5)/5 ) ::
-    Vec3( 0,-1/2-3*math.sqrt(5)/10,-1-2*math.sqrt(5)/5 ) ::
-    Vec3( 1+2*math.sqrt(5)/5, 0, -1/2-3*math.sqrt(5)/10 ) :: Nil
+  val A = Vec3( -1/gold,0,-gold ) // 0
+  val B = Vec3( 1/gold,0,-gold ) // 1
+  val C= Vec3( 1,1,-1 ) // 2
+  val Cp = Vec3( -1,1,-1 ) // 3
+  val D = Vec3( 1,-1,-1 ) // 4
+  val Dp = Vec3( -1,-1,-1 ) // 5
+  val E = Vec3( gold,1/gold,0 ) // 6
+  val F = Vec3( gold,-1/gold,0 ) // 7
+  val G = Vec3( 0,gold,-1/gold ) // 8
+  val H = Vec3( 0,-gold,-1/gold ) // 9
+  val I = ( A + B ) / 2 // 10
+  val J = ( C + B ) / 2 // 11
+  val K = ( B + D ) / 2 // 12
+  val M = ( A + B + C + Cp + G ) / 5 // 13
+  val N = ( A + B + D + Dp + H ) / 5 // 14
+  val O = ( B + C + D + E + F ) / 5 // 15
+  val vertices: List[Vertex] = A :: B :: C :: Cp :: D :: Dp :: E :: F :: G :: H ::
+    I :: J :: K :: M :: N :: O ::
+    Nil
 
   val facesStructure =
     List(
-      ( List( 0 , 1 , 4, 2 ), DDCSFace, List ( 0 ) ),
-      ( List( 0 , 2 , 6, 3 ), DDCSFace, List ( 0 ) ),
-      ( List( 0 , 3 , 5, 1 ), DDCSFace, List ( 0 ) ),
-      ( List( 1 , 5 , 4 ), DDCSSide, List ( 0 ) ),
-      ( List( 2 , 4 , 6 ), DDCSSide, List ( 0 ) ),
-      ( List( 3 , 6 , 5 ), DDCSSide, List ( 0 ) ),
-      ( List( 4 , 5 , 6 ), RegularPolygon( 3 ), List( 0 ) ) )
+      // ( List(2,6,7,4,1), RegularPolygon( 5 ), List( 0 ) ),
+      // ( List(3,8,2,1,0), RegularPolygon( 5 ), List( 0 ) ),
+      // ( List(4,9,5,0,1), RegularPolygon( 5 ), List( 0 ) ),
+      ( List(13,14,15), RegularPolygon( 3 ), List( 0 ) ), // MNO
+      ( List( 1 , 10 , 13, 11 ), DDCSFace, List ( 0 ) ), // BIMJ
+      ( List( 1 , 11 , 15, 12 ), DDCSFace, List ( 0 ) ), // BJOK
+      ( List( 1 , 12 , 14, 10 ), DDCSFace, List ( 0 ) ), // BKNI
+      ( List( 10 , 14 , 13 ), DDCSSide, List ( 0 ) ),    // INM
+      ( List( 11 , 13 , 15 ), DDCSSide, List ( 0 ) ),    // JMO
+      ( List( 12 , 15 , 14 ), DDCSSide, List ( 0 ) )    // KON
+    )
 }
